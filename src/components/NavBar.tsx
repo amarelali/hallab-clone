fc31587485b06a1862f72a9415f13387801f1ec5
amarelali <amar.elali@isae.edu.lb>
2025-02-25 13:25:09 +0200

    enhance style of header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,13 +5,32 @@ import {
   faLinkedin,
   faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { faShoppingCart,faSearch, faHeart } from "@fortawesome/free-solid-svg-icons"; // ✅ Correct import
+import {
+  faShoppingCart,
+  faSearch,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons"; 
 
 import Select from "./ui/Select";
 import { useState } from "react";
 
 interface IProps {}
 const NavBar = ({}: IProps) => {
+  const navItems = [
+    { label: "Home", link: "#" },
+    { label: "Shop Online", link: "#" },
+    { label: "Gift Ideas", link: "#" },
+    { label: "Branch", link: "#" },
+    { label: "Catering", link: "#" },
+    { label: "Menus", link: "#" },
+    { label: "Contact us", link: "#" },
+  ];
+  const socialMediaLinks = [
+    { icon: faFacebookF, url: "https://www.facebook.com", name: "Facebook" },
+    { icon: faInstagram, url: "https://www.instagram.com", name: "Instagram" },
+    { icon: faXTwitter, url: "https://twitter.com", name: "Twitter" },
+    { icon: faLinkedin, url: "https://www.linkedin.com", name: "LinkedIn" },
+  ];
   const [searchText, setSearchText] = useState<string>("");
   const [displaySearchInput, setDisplaySearchInput] = useState<boolean>(false);
   return (
@@ -20,36 +39,67 @@ const NavBar = ({}: IProps) => {
         <div className="w-5/6 m-auto">
           <div className="flex justify-between items-end">
             <div className="social-media flex gap-4">
-              <FontAwesomeIcon icon={faFacebookF} size="1x" />
-              <FontAwesomeIcon icon={faInstagram} size="1x" />
-              <FontAwesomeIcon icon={faXTwitter} size="1x" />
-              <FontAwesomeIcon icon={faLinkedin} size="1x" />
+              {socialMediaLinks.map(({ url, name, icon }, index) => (
+                <a
+                  key={index}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer" 
+                  //**  noopener : make sure that next website is not able to access information about the previous website
+                  //** noreferrer : "privacy" don't make the site visited know from when i come from
+                  aria-label={name}
+                >
+                  <FontAwesomeIcon icon={icon} size="1x" />
+                </a>
+              ))}
             </div>
             <img src="assets/Lebanon-Store.png" className="logo" />
             <div className="w-40 flex justify-end">
               <Select />
             </div>
           </div>
-          <hr className="my-2 pb-2" />
-          <div className="flex justify-between">
-          <ul className="flex gap-4 pb-2">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Shop Online</a></li>
-            <li><a href="#">Gift Ideas</a></li>
-            <li><a href="#">Branch</a></li>
-            <li><a href="#">Catering</a></li>
-            <li><a href="#">Menus</a></li>
-            <li><a href="#">Contact us</a></li>
-          </ul>
-
-          <ul className="flex gap-5 pb-2 items-center">
-
-            <li> {displaySearchInput && <input type="text" className="rounded-sm text-black" value={searchText} onChange={(e)=> setSearchText(e.target.value)} />} <a href="#" onClick={()=>{ setDisplaySearchInput((prev)=> !prev)}}> <FontAwesomeIcon icon={faSearch} /></a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faHeart} /> <sup>0</sup></a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faShoppingCart} /> <sup>0</sup></a></li>
-            <li><a href="#">Sign in</a></li>
-          </ul>
-
+          <hr className="my-3 pb-3" />
+          <div className="flex justify-between gap-3">
+            <ul className="flex gap-4 ">
+              {navItems.map(({ link, label }, index) => (
+                <li key={index}>
+                  <a href={link}>{label}</a>
+                </li>
+              ))}
+            </ul>
+            <ul className="flex gap-5 items-center pb-3">
+              <li>
+                {displaySearchInput && (
+                  <input
+                    type="text"
+                    className="rounded-sm text-black"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                  />
+                )}
+                <a
+                  href="#"
+                  onClick={() => {
+                    setDisplaySearchInput((prev) => !prev);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSearch} />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FontAwesomeIcon icon={faHeart} /> <sup>0</sup>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FontAwesomeIcon icon={faShoppingCart} /> <sup>0</sup>
+                </a>
+              </li>
+              <li>
+                <a href="#">Sign in</a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
