41cd90048b56f8a7744f8f05d8a49a73f9c5436d
amarelali <amar.elali@isae.edu.lb>
2025-03-06 14:22:35 +0200

    add custom style

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,13 @@
 interface IProps {
     url?: string;
     children: React.ReactNode;
+    style?:string
 }
-const CustomButton= ({url="",children}: IProps) => {
+const CustomButton= ({url="",children,style}: IProps) => {
   return (
     <a
     href={url}
-    className="bg-[#006648] text-white w-fit px-8 py-3 rounded-md " //hover:bg-green-700 transition
+    className={`bg-[#006648] text-white w-fit px-8 py-3 rounded-md ${style}`} //hover:bg-green-700 transition
   >
     {children}
   </a>
